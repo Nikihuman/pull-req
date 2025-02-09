dc5cd17fd1aa2cf583f7f5648db8587a3335cf17
Mikita <bansanrock@gmail.com>
2025-02-09 11:02:58 +0300

    feat: random values

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import { IEnvInterfaces } from "./interfaces/env-arrays.interface";
+import { RandomValues } from "./random-values/random-values.class";
+
+function bootstrap() {
+  const env = process.env.ARRAY_OF_NUMBERS;
+
+  if (!env) {
+    throw new Error(`No such env`);
+  }
+
+  const { first_array, second_array }: IEnvInterfaces = JSON.parse(env);
+
+  const randomValueFirst = new RandomValues({
+    from: first_array[0],
+    to: first_array[1],
+  }).getRandomValue();
+
+  const randomValueSecond = new RandomValues({
+    from: second_array[0],
+    to: second_array[1],
+  }).getRandomValue();
+
+  console.log(`First random value: ${randomValueFirst}`);
+  console.log(`Second random value: ${randomValueSecond}`);
+}
+
+bootstrap();
